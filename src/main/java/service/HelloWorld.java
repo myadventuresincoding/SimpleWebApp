a7852e954dfde9cec12ccc3860f4ab172ab8d9cf
myadventuresincoding <66219262+myadventuresincoding@users.noreply.github.com>
2020-05-31 12:42:07 -0600

    Initial commit of example web app

---
+++ src/main/java/service/HelloWorld.java
@@ -0,0 +1,17 @@
+package service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET
+    @Path("world")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMsg() {
+        return "Hello World - REST!";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World - JSP!</h2>
+<h3>Now try the REST URL</h3>
+<p>/rest/hello/world</p>
+</body>
+</html>
